59c2a0825b07526c92415265592b6a9b6e4ff888
shetty-nithin <shettynithin744@gmail.com>
2022-06-25 01:35:05 +0530

    10th commit : 25th June 2022

---
+++ 44_Minimum_number_of_Platform.cpp
@@ -0,0 +1,62 @@
+// Link : 
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute Force Approach:
+ 
+bool static comparator(pair<int, int> n1, pair<int, int> n2){
+    if(n1.second < n2.second){
+        return true;
+    }
+    return false;
+}
+
+int calculateMinPatforms(int at[], int dt[], int n) {
+    vector<pair<int, int>> scheduleArr;
+    for(int i=0; i<n; i++){
+        scheduleArr.push_back({at[i], dt[i]});
+    }
+    sort(scheduleArr.begin(), scheduleArr.end(), comparator);
+    
+    int minPlatform = 1;
+    int deptTime;
+    for(int i=0; i<n-1; i++){
+        deptTime = scheduleArr[i].second;
+        int temp = 1;
+        for(int j=i+1; j<n; j++){
+            if(scheduleArr[j].first <= deptTime){
+                temp++;
+            }
+        }
+        minPlatform = max(minPlatform, temp);
+    }
+    
+    return minPlatform;
+}
+
+// approach 2:
+#include<algorithm>
+int calculateMinPatforms(int at[], int dt[], int n) {
+    sort(at, at+n);
+    sort(dt, dt+n);
+    
+    int minPlatform = 1;
+    int temp = 1;
+    int i = 0;
+    int j = 1;
+    while(j<n && i<n){
+        if(at[j] <= dt[i]){
+            temp++;
+            j++;
+        }else{
+            temp--;
+            i++;
+        }
+        minPlatform = max(minPlatform, temp);
+    }
+    
+    return minPlatform;
+} 
---
+++ 45_Job_Sequencing.cpp
@@ -0,0 +1,53 @@
+// Link : https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1#
+
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Job 
+{ 
+    int id;	 // Job Id 
+    int dead; // Deadline of job 
+    int profit; // Profit if job is over before or on deadline 
+};
+
+class Solution 
+{
+    public:
+    bool static comparison(Job a, Job b) {
+        return (a.profit > b.profit);
+    }
+
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        
+        int maxDeadline = 0;
+        for (int i = 0; i < n; i++) {
+            maxDeadline = max(maxDeadline, arr[i].dead);
+        }
+
+        sort(arr, arr + n, comparison);
+
+        int slot[5+1]; //int slot[maxDeadline+1];
+        for(int i=0; i<=maxDeadline; i++){
+            slot[i] = -1;
+        }
+        
+        int maxProfit = 0;
+        int maxJobs = 0;
+        for(int i=0; i<n; i++){
+            for(int j=arr[i].dead; j>0; j--){
+                if(slot[j] == -1){
+                    slot[j] = i;
+                    maxProfit += arr[i].profit;
+                    maxJobs++;
+                    break;
+                }
+            }
+        }
+        
+        return {maxJobs, maxProfit};
+    } 
+};
