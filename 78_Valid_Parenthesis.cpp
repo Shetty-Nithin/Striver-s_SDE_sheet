944f1bfe72df50540fd203158ed0da105f57784d
shetty-nithin <shettynithin744@gmail.com>
2022-07-07 00:06:45 +0530

    20th commit : 7th July 2022

---
+++ 78_Valid_Parenthesis.cpp
@@ -0,0 +1,27 @@
+// Link : https://leetcode.com/problems/valid-parentheses/submissions/
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        
+        for(int i=0; i<s.length(); i++){
+            if(!st.empty() && (st.top() == '{' && s[i] == '}' || 
+                               st.top() == '(' && s[i] == ')' || 
+                               st.top() == '[' && s[i] == ']')){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        
+        if(st.empty()) return true;
+        return false;
+    }
+};
